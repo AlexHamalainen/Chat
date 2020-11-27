ec7e5eb1691a443befb91968495102b0fa87bafb
Alex Hämäläinen <72603771+AlexHamalainen@users.noreply.github.com>
2020-11-27 10:20:39 +0200

    Update hae.php

--- hae.php
+++ hae.php
@@ -1,18 +1,27 @@
 <?php
-include ("../../../jotaki.php");
-// Create connection
+// vaihda tähän oman tietokantasi tiedot tai tuo ne ulkoisesta tiedostosta include toiminnolla.
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database name";
+
+// Luo tietokantayhteys
 $conn = new mysqli($servername, $username, $password, $dbname);
+// unohda käyttäjänimi ja salasana koska niitä ei enää tarvita
 unset ($username, $password);
-// Check connection
+// tarkista yhteys
 if ($conn->connect_error) {
   die("Yhteys ei onnistunut: " . $conn->connect_error);
 }
 
+// valitaan taulu ja sen kentät
 $sql = "SELECT Viesti, Aika, Nimi FROM chat";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  // output data of each row
+  // jos näytettävää on, näytetään se
+  // Aika tulee lihavoituna, nimimerkki sinisenä ja niiden perään viesti ja rivinvaihto
   while($row = $result->fetch_assoc()) {
     echo "<b>" . $row["Aika"] . ":</b> <span style='color: blue;'>@" . $row['Nimi'] . "</span> " . $row["Viesti"] . "<br>";
   }
